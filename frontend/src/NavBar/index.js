3062a5e05e43a90fd94ae1968153a41e70c867dc
Sandy Cruz <sandyleahcruz@icloud.com>
2021-04-05 12:12:13 -0700

    Add ESLint and initial github action

--- frontend/src/NavBar/index.js
+++ frontend/src/NavBar/index.js
@@ -2,12 +2,10 @@ import React from "react";
 
 import Logo from "./Logo.svg";
 
-const NavBar = () => {
-  return (
-    <div>
-      <Logo />
-    </div>
-  );
-};
+const NavBar = () => (
+  <div>
+    <Logo />
+  </div>
+);
 
 export default NavBar;
