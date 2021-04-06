ac46f4909c1fdc45f672aa63e5a584c952c015d0
Sandy Cruz <sandyleahcruz@icloud.com>
2021-04-06 13:45:23 -0700

    Fix rubocop errors

---
+++ backend/app/models/user.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  attr_reader :password
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  after_initialize :ensure_session_token
+
+  has_many :favorites,
+           primary_key: :id,
+           foreign_key: :user_id
+end
--- backend/db/migrate/20210406201150_add_users_table.rb
+++ backend/db/migrate/20210406201150_add_users_table.rb
@@ -1,12 +1,15 @@
+# frozen_string_literal: true
+
+# Create users table migration
 class AddUsersTable < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :username, null: false
       t.string :email, null: false
       t.string :password_digest, null: false
-      t.string :session_token, null: false     
+      t.string :session_token, null: false
       t.datetime :created_at, null: false
-      t.datetime :updated_at, null: false 
+      t.datetime :updated_at, null: false
     end
 
     add_index :users, :username, unique: true
