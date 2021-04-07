a7bfddea7b2e377e71e70d60d898faac487b38fd
Sandy Cruz <sandyleahcruz@icloud.com>
2021-04-06 18:58:28 -0700

    Add entities reducer

---
+++ frontend/reducers/users_reducer.js
@@ -0,0 +1,17 @@
+import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
+
+const usersReducer = (state = {}, action) => {
+  Object.freeze(state);
+
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return {
+        ...state,
+        [action.currentUser.id]: { ...action.currentUser, isCurrentUser: true },
+      };
+    default:
+      return state;
+  }
+};
+
+export default usersReducer;
