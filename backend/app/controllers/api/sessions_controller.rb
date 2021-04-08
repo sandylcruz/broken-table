05faf84174ae350c737a4dfc648a22fc400ffe5c
Sandy Cruz <sandyleahcruz@icloud.com>
2021-04-07 18:50:23 -0700

    Address further comments

--- backend/app/controllers/api/sessions_controller.rb
+++ backend/app/controllers/api/sessions_controller.rb
@@ -5,9 +5,7 @@ module Api
   class SessionsController < ApplicationController
     skip_before_action :verify_authenticity_token
 
-    def new
-      render :new
-    end
+    def new; end
 
     def create
       @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
--- backend/db/migrate/20210406201150_add_users_table.rb
+++ backend/db/migrate/20210406201150_add_users_table.rb
@@ -14,5 +14,6 @@ class AddUsersTable < ActiveRecord::Migration[6.1]
 
     add_index :users, :username, unique: true
     add_index :users, :email, unique: true
+    add_index :users, :session_token, unique: true
   end
 end
