496ec5b85ff6abd594b3f9c4279ba1b993a0797a
Sandy Cruz <sandyleahcruz@icloud.com>
2021-04-09 19:03:28 -0700

    Write create userscontroller test

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -5,16 +5,11 @@ module Api
   class UsersController < ApplicationController
     skip_before_action :verify_authenticity_token
 
-    def new
-      @user = User.new(user_params)
-    end
-
     def create
       @user = User.new(user_params)
 
       if @user.save
         login!(@user)
-        render 'api/users/show'
       else
         render json: @user.errors.full_messages, status: 422
       end
--- backend/spec/controllers/api/users_controller_spec.rb
+++ backend/spec/controllers/api/users_controller_spec.rb
@@ -3,19 +3,28 @@
 require 'rails_helper'
 
 RSpec.describe Api::UsersController, type: :controller do
-  describe 'GET #new' do
+  describe 'GET #create' do
     it 'should instantiate a new user given valid params' do
-      user :create, params: { user: { username: 'calliecat', password: 'password' } }
-      expect(response).to have_http_status(:ok)
+      post :create, params: { user: { username: 'calliecat2', password: 'password', email: 'cat@gmail.com' } }
     end
 
     it 'should render an error if a username or password is not given' do
+      post :create, params: { user: { username: '', password: 'password', email: 'cat@gmail.com' } }
+      post :create, params: { user: { username: 'calliecat', password: '', email: 'cat@gmail.com' } }
     end
 
     it 'should validate password is at leaset 6 characters long' do
+      post :create, params: { user: { username: 'calliecat', password: 'a', email: 'cat@gmail.com' } }
+    end
+  end
+
+  describe 'GET #show' do
+    it 'should return a user if given valid user id' do
+      post :show, params: { user: { id: 1 } }
+      it 'shoulld render'
     end
 
-    it 'should redirect to root page if user is successfully created' do
+    it 'should not return a 404 if given invalid user id' do
     end
   end
 end
