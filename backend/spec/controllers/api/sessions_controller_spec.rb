8be6fcaef0b899a5a9c6b9790d2fe08161fc2bf9
Sandy Cruz <sandyleahcruz@icloud.com>
2021-04-10 12:16:37 -0700

    Finish sessions controller spec

--- backend/spec/controllers/api/sessions_controller_spec.rb
+++ backend/spec/controllers/api/sessions_controller_spec.rb
@@ -3,4 +3,27 @@
 require 'rails_helper'
 
 RSpec.describe Api::SessionsController, type: :controller do
+  let(:valid_user) { User.new(username: 'calliethecat', password: 'password', email: "calliethecat@gmail.com") }
+
+  describe 'POST #create' do
+    it 'should create a new session if user credentials are valid' do
+      user = valid_user
+      user.save!
+      original_session_token = user.session_token
+
+      post :create, params: { user: { username: 'calliethecat', password: 'password' } }, format: :json
+      response.should render_template :show
+
+      user.reload
+      new_session_token = user.session_token
+
+      expect(new_session_token).not_to eq(original_session_token)
+      expect(session[:session_token]).to eq(new_session_token)
+    end
+
+    it 'should render an error if a user is nil' do
+      post :create, params: { user: { username: 'calliethecat', password: 'bad_password' } }
+      expect(response).to have_http_status(401)
+    end
+  end
 end
--- backend/spec/controllers/api/users_controller_spec.rb
+++ backend/spec/controllers/api/users_controller_spec.rb
@@ -6,15 +6,17 @@ RSpec.describe Api::UsersController, type: :controller do
   describe 'GET #create' do
     it 'should instantiate a new user given valid params' do
       post :create, params: { user: { username: 'calliecat2', password: 'password', email: 'cat@gmail.com' } }
+      assert_response :success
     end
 
     it 'should render an error if a username or password is not given' do
       post :create, params: { user: { username: '', password: 'password', email: 'cat@gmail.com' } }
-      post :create, params: { user: { username: 'calliecat', password: '', email: 'cat@gmail.com' } }
+      expect(response).to have_http_status(422)
     end
 
     it 'should validate password is at leaset 6 characters long' do
-      post :create, params: { user: { username: 'calliecat', password: 'a', email: 'cat@gmail.com' } }
+      post :create, params: { user: { username: 'calliecat', password: '', email: 'cat@gmail.com' } }
+      expect(response).to have_http_status(422)
     end
   end
 end
