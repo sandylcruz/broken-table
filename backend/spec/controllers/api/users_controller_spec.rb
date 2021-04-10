7868f77ea84ee0d4475f30cc8dd0d92969effb53
Sandy Cruz <sandyleahcruz@icloud.com>
2021-04-09 17:55:05 -0700

    Start writing to controller tests

--- backend/spec/controllers/api/users_controller_spec.rb
+++ backend/spec/controllers/api/users_controller_spec.rb
@@ -3,6 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe Api::UsersController, type: :controller do
-  describe 'new' do
+  describe 'GET #new' do
+    it 'should instantiate a new user given valid params' do
+      user :create, params: { user: { username: 'calliecat', password: 'password' } }
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should render an error if a username or password is not given' do
+    end
+
+    it 'should validate password is at leaset 6 characters long' do
+    end
+
+    it 'should redirect to root page if user is successfully created' do
+    end
   end
 end
