814bbb7161b5093a36e4d39585b952c91e9c139a
Sandy Cruz <sandyleahcruz@icloud.com>
2021-04-10 19:59:56 -0700

    Seed restaurant database

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -9,3 +9,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create!(username: 'calpal', password: 'password', email: 'calpal@gmail.com')
+User.create!(username: 'squeakfreak', password: 'password', email: 'squeaks@gmail.com')
+User.create!(username: 'flapjack', password: 'password', email: 'flapjack@gmail.com')
+
+Restaurant.create!(name: 'Nopa', description: 'A San Francisco Gathering Place',
+                   location: '560 Divisadero St, SF, CA, 94117')
+Restaurant.create!(name: `L'Ardoise`, description: 'French bistro in San Francisco',
+                   location: '151 Noe St, SF, CA 94114')
+Restaurant.create!(name: 'Zuni Cafe', description: 'Zuni Cafe opened in San Francisco on Febuarary 15, 1979',
+                   location: '1658 Market St, SF, CA 94102')
