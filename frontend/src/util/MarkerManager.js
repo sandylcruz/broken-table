ed0babae702e77ee9b298e365ff1bfadd11924c3
Sandy Cruz <sandyleahcruz@icloud.com>
2021-04-15 10:32:18 -0700

    Write function to create marker from a new restaurant

--- frontend/src/util/MarkerManager.js
+++ frontend/src/util/MarkerManager.js
@@ -6,27 +6,29 @@ class MarkerManager {
     this.updateMarkers = this.updateMarkers.bind(this);
   }
 
-  updateMarkers(restaurants) {
-    const restaurantsObject = {};
+  // updateMarkers(restaurants) {
+  //   const restaurantsObject = {};
 
-    restaurants.forEach((restaurant) => {
-      restaurantsObject[restaurant.id] = restaurant;
+  //   restaurants.forEach((restaurant) => {
+  //     restaurantsObject[restaurant.id] = restaurant;
+  //   });
+
+  //   this.markers;
+  //   // restaurantsObject.keys((restaurant) => {});
+  // }
+
+  createMarkerFromRestaurant(restaurant) {
+    const myLatLng = { lat: restaurant.latitude, lng: restaurant.longitude };
+
+    const newMarker = new google.maps.Marker({
+      position: myLatLng,
+      map: this.map,
+      title: restaurant.name,
     });
-
-    restaurants.forEach((restaurant) => {
-      const myLatLng = { lat: restaurant.latitude, lng: restaurant.longitude };
-      const currentMarker = this.markers[restaurant.id];
-
-      if (!currentMarker) {
-        const newMarker = new google.maps.Marker({
-          position: myLatLng,
-          map: this.map,
-          title: restaurant.name,
-        });
-        this.markers[restaurant.id] = newMarker;
-      }
-    });
+    this.markers[restaurant.id] = newMarker;
   }
+
+  // removeMarkers(marker) {}
 }
 
 export default MarkerManager;
