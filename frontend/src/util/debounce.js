2488189a5d76f160774985d82a93f6f55b82bb95
Sandy Cruz <sandyleahcruz@icloud.com>
2021-04-15 19:22:56 -0700

    Start writing debounce

--- frontend/src/util/debounce.js
+++ frontend/src/util/debounce.js
@@ -1,8 +1,18 @@
+import { useDispatch } from "react";
+
 // const myFunction = () => {}
 
 // const myDebounceFunction = debounce(myFunction, 500)
 // myDebounceFunction() <- is a function
 
-// const debounce = (myFunction, time) => {
-//   return () => {};
-// };
+const debounce = (func, timeout) => {
+  const timer = setTimeout(() => {}, timeout);
+
+  if (!timer) {
+    useDispatch(func);
+    clearTimeout(timer);
+  }
+  return () => {};
+};
+
+export default debounce;
