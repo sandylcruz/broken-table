58557e251391d0a45f04443998ec989a0c4b084d
Sandy Cruz <sandyleahcruz@icloud.com>
2021-04-17 19:04:19 -0700

    Start working on restauant form

--- frontend/src/RestaurantForm/RestaurantForm.js
+++ frontend/src/RestaurantForm/RestaurantForm.js
@@ -1,13 +1,38 @@
-import React from "react";
+// import React from "react";
+// // import Geocode from "react-geocode";
 
-const RestaurantForm = React.memo(() => {
-  // const [name, setName] = useState("");
-  // const [description, setDescription] = useState("");
-  // const [location, setLocation] = useState("");
-  // const [latitude, setLatitude] = useState("");
-  // const [longitude, setLongitude] = useState("");
-  // const [submitter, setSubmitter] = useState("");
-  // return <div></div>;
-});
+// // user inputs address
+// // map is generated which generates lat/lng
+// // save lat/lng into state
+// // const getCoordsFromAddress = (address) => {
+// // Geocode.fromAddress(address).then(
+// //   (response) => {
+// //     const { latitude, longitude } = response.results[0].geometry.location;
+// //     console.log(latitude, longitude);
+// //   },
+// //   (error) => {
+// //     console.errors(error);
+// //   }
+// // );
+// // };
 
-export default RestaurantForm;
+// const RestaurantForm = React.memo(() =>
+//   // const [name, setName] = useState("");
+//   // const [description, setDescription] = useState("");
+//   // const [location, setLocation] = useState("");
+//   // const [latitude, setLatitude] = useState("");
+//   // const [longitude, setLongitude] = useState("");
+//   // const [submitter, setSubmitter] = useState("");
+
+//   // const a = console.log("hi");
+
+//   return (
+//     <div>
+//       <form onSubmit={submitForm}>
+//         <h1>Create a New Restaurant</h1>
+//       </form>
+//     </div>
+//   );
+// );
+
+// export default RestaurantForm;
---
+++ frontend/src/RestaurantForm/RestaurantFormContainer.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+import RestaurantForm from "./RestaurantForm";
+
+const RestaurantFormContainer = (props) => <RestaurantForm props={props} />;
+export default RestaurantFormContainer;
