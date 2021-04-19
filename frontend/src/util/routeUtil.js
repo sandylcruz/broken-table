a3d0f0c1105324ada44b4be5f5f10f6612ce2b90
Sandy Cruz <sandyleahcruz@icloud.com>
2021-04-18 18:42:44 -0700

    Protect route to create restaurant

--- frontend/src/util/routeUtil.js
+++ frontend/src/util/routeUtil.js
@@ -20,14 +20,17 @@ const Auth = React.memo(({ component: Component, path, exact }) => {
 
 export const AuthRoute = Auth;
 
-const Protected = ({ component: Component, path, loggedIn, exact }) => (
-  <Route
-    path={path}
-    exact={exact}
-    render={(props) =>
-      loggedIn ? <Component {...props} /> : <Redirect to="/login" />
-    }
-  />
-);
+const Protected = React.memo(({ component: Component, path, exact }) => {
+  const currentUser = useSelector(selectCurrentUser);
+  return (
+    <Route
+      path={path}
+      exact={exact}
+      render={(props) =>
+        currentUser ? <Component {...props} /> : <Redirect to="/login" />
+      }
+    />
+  );
+});
 
 export const ProtectedRoute = Protected;
