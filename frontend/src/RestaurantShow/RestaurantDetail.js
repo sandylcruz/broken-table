6a5ed7ff2075968d0e3297943ed1bd268dbdefe8
Sandy Cruz <sandyleahcruz@icloud.com>
2021-04-20 19:05:22 -0700

    Add more css styling

--- frontend/src/RestaurantShow/RestaurantDetail.js
+++ frontend/src/RestaurantShow/RestaurantDetail.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
 
 import food from "./food.png";
@@ -42,37 +42,69 @@ const StyledRestaurantSummary = styled.div`
   padding: 10px;
 `;
 
-const RestaurantDetail = React.memo(({ restaurant }) => (
-  <StyledParentContainer>
-    {!restaurant ? (
-      <div>Loading...</div>
-    ) : (
-      <StyledRestaurantOverview>
-        <h1>{restaurant.name}</h1>
-        <StyledStar />
-        <h3>About {restaurant.name} </h3>
-        <p>{restaurant.description}</p>
-        <StyledReservationContainer>
-          Make a reservation
-        </StyledReservationContainer>
-      </StyledRestaurantOverview>
-    )}
+// const loader = new Loader({
+//   apiKey: MAPS_API_KEY,
+//   version: "weekly",
+//   ...additionalOptions,
+// });
 
-    <StyledSideBar>
+const RestaurantDetail = React.memo(({ restaurant }) => {
+  const mapNodeRef = useRef();
+  const mapRef = useRef();
+
+  useEffect(() => {
+    if (restaurant) {
+      const mapLocation = {
+        center: {
+          lat: `${restaurant.latitude}`,
+          lng: `${restaurant.latitude}`,
+        },
+        zoom: 13,
+      };
+
+      const map = new google.maps.Map(mapNodeRef.current, mapLocation);
+      mapRef.current = map;
+
+      return () => {
+        mapRef.current = undefined;
+      };
+    }
+    return true;
+  }, [restaurant]);
+
+  return (
+    <StyledParentContainer>
       {!restaurant ? (
         <div>Loading...</div>
       ) : (
-        <div>
-          {" "}
-          <img src={food} height="300" width="300" alt="food" />
-          <StyledRestaurantSummary>
-            <h3>{restaurant.name}</h3>
-            <span>{restaurant.location}</span>
-          </StyledRestaurantSummary>
-        </div>
+        <StyledRestaurantOverview>
+          <h1>{restaurant.name}</h1>
+          <StyledStar />
+          <h3>About {restaurant.name} </h3>
+          <p>{restaurant.description}</p>
+          <StyledReservationContainer>
+            Make a reservation
+          </StyledReservationContainer>
+        </StyledRestaurantOverview>
       )}
-    </StyledSideBar>
-  </StyledParentContainer>
-));
+
+      <StyledSideBar>
+        {!restaurant ? (
+          <div>Loading...</div>
+        ) : (
+          <div>
+            {" "}
+            <img src={food} height="300" width="300" alt="food" />
+            <StyledRestaurantSummary>
+              <h3>{restaurant.name}</h3>
+              <span>{restaurant.location}</span>
+              <div ref={mapNodeRef} />
+            </StyledRestaurantSummary>
+          </div>
+        )}
+      </StyledSideBar>
+    </StyledParentContainer>
+  );
+});
 
 export default RestaurantDetail;
