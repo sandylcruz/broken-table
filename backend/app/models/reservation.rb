8cd12ce7f349c4cdae9e3b5fb06a6cea5259e006
Sandy Cruz <sandyleahcruz@icloud.com>
2021-04-25 13:19:36 -0700

    Write reservation validations and associations

--- backend/app/models/reservation.rb
+++ backend/app/models/reservation.rb
@@ -7,4 +7,14 @@ class Reservation < ApplicationRecord
   validates :status, presence: true
   validates :created_at, null: false
   validates :updated_at, null: false
+
+  belongs_to :user,
+             class_name: 'User',
+             foreign_key: :user_id,
+             primary_key: id
+
+  belongs_to :restaurant,
+             class_name: 'Restaurant',
+             foreign_key: :restaurant_id,
+             primary_key: id
 end
--- backend/app/models/restaurant.rb
+++ backend/app/models/restaurant.rb
@@ -14,6 +14,11 @@ class Restaurant < ApplicationRecord
              foreign_key: :submitter_id,
              primary_key: :id
 
+  has_many :reservations,
+           class_name: 'Reservation',
+           foreign_key: :restaurant_id,
+           primary_key: :id
+
   has_one_attached :photo
 
   def self.in_bounds(bounds)
