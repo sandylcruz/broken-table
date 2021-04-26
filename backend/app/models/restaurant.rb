7ceece90eeef7d3463b4e97c4078a64e1d3b4993
Sandy Cruz <sandyleahcruz@icloud.com>
2021-04-26 11:41:57 -0700

    Write associations between restaurants, reviews, and users

--- backend/app/models/restaurant.rb
+++ backend/app/models/restaurant.rb
@@ -19,6 +19,11 @@ class Restaurant < ApplicationRecord
            foreign_key: :restaurant_id,
            primary_key: :id
 
+  has_many :reviews,
+           class_name: 'Review',
+           foreign_key: :restaurant_id,
+           primary_key: :id
+
   has_one_attached :photo
 
   def self.in_bounds(bounds)
--- backend/app/models/reviews.rb
+++ backend/app/models/reviews.rb
@@ -4,5 +4,13 @@ class Reviews < ApplicationRecord
   validates :rating, null: false
   validates :body, null: false
 
-  belongs_to :user
+  belongs_to :author,
+             class_name: 'User',
+             foreign_key: :user_id,
+             primary_key: :id
+
+  belongs_to :restaurant,
+             class_name: 'Restaurant',
+             foreign_key: :restaurant_id,
+             primary_key: :id
 end
