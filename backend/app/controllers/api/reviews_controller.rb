4423136bc81a6c5f0ff4e1553f0195938286c467
Sandy Cruz <sandyleahcruz@icloud.com>
2021-04-26 11:59:13 -0700

    Write reviews controller

--- backend/app/controllers/api/reviews_controller.rb
+++ backend/app/controllers/api/reviews_controller.rb
@@ -1,4 +1,23 @@
 # frozen_string_literal: true
 
-class ReviewsController < ApplicationController
+module Api
+  # Reviews Controller class
+  class ReviewsController < ApplicationController
+    def create
+      @review = Review.new(review_params)
+      @review.user_id = current_user.id
+
+      if @review.save
+        render :show
+      else
+        render json: @review.errors.full_messages, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def review_params
+      params.require(:review).permit(:restaurant_id, :user_id, :rating, :body)
+    end
+  end
 end
