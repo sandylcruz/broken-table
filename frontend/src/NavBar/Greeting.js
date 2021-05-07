73be25d71a893ea2aefe4c208133363fa5e76eb3
Sandy Cruz <sandyleahcruz@icloud.com>
2021-05-07 15:08:19 -0700

    Add user avatar as button instead of greeting and logout button

--- frontend/src/NavBar/Greeting.js
+++ frontend/src/NavBar/Greeting.js
@@ -4,25 +4,55 @@ import styled from "styled-components";
 
 import PrimaryButton from "../components/PrimaryButton";
 import SecondaryButton from "../components/SecondaryButton";
-import LogoutButton from "../components/LogoutButton";
+// import LogoutButton from "../components/LogoutButton";
 
-const GreetingAndLogoutDiv = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: right;
-  margin-right: 20px;
+const DropdownContentA = styled.a`
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
 `;
 
-const GreetingText = styled.p`
-  font-size: 15px;
-  font-family: helvetica;
-  margin-top: 5px;
+const DropdownContentDiv = styled.div`
+  display: none;
+  position: absolute;
+  background-color: pink;
+  // background-color: #f1f1f1;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  z-index: 1;
 `;
 
-const StyledAvatar = styled.img`
+const DropdownDiv = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
+const StyledAvatarInput = styled.input`
   height: 50px;
   width: 50px;
   border-radius: 50%;
+
+  &:hover {
+    box-shadow: #c6c6c6 0px 0px 0px 4px;
+    transition: border-color 0.35s ease-in-out 0s,
+      box-shadow 0.2s ease-in-out 0s, background-color 0.25s ease-in-out 0s,
+      color 0.25s ease-in-out 0s;
+  }
+
+  &:active {
+    box-shadow: #adadad 0px 0px 0px 5px;
+    transition: border-color 0.25s ease-in-out 0s,
+      box-shadow 0.1s ease-in-out 0s, background-color 0.25s ease-in-out 0s,
+      color 0.25s ease-in-out 0s;
+  }
+
+  &:focus {
+    box-shadow: #a0a0a0 0px 0px 0px 6px;
+    transition: border-color 0.25s ease-in-out 0s,
+      box-shadow 0.1s ease-in-out 0s, background-color 0.25s ease-in-out 0s,
+      color 0.25s ease-in-out 0s;
+  }
 `;
 
 const StyledNav = styled.nav`
@@ -50,13 +80,24 @@ const Greeting = React.memo(({ currentUser, logout }) => {
         </div>
       ) : (
         <div>
-          <GreetingAndLogoutDiv>
-            <StyledAvatar src={currentUser.photoUrl} alt="" />
-            <GreetingText>Hi, {currentUser.username}!</GreetingText>
-            <LogoutButton type="submit" onClick={logout}>
-              Log Out
-            </LogoutButton>
-          </GreetingAndLogoutDiv>
+          <DropdownDiv>
+            <StyledAvatarInput
+              type="image"
+              src={currentUser.photoUrl}
+              alt="user-photo"
+            />
+            <DropdownContentDiv>
+              <DropdownContentA href="#">Create a Restaurant</DropdownContentA>
+              <DropdownContentA href="#" onClick={logout}>
+                Logout
+              </DropdownContentA>
+            </DropdownContentDiv>
+
+            {/* <GreetingText>Hi, {currentUser.username}!</GreetingText> */}
+            {/* <LogoutButton type="submit" onClick={logout}> */}
+            {/* Log Out
+            </LogoutButton> */}
+          </DropdownDiv>
         </div>
       )}
     </StyledNav>
