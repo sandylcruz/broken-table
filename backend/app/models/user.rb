ef988895e7cf2932388b51d13c233ddad393fc61
Sandy Cruz <sandyleahcruz@icloud.com>
2021-05-06 17:44:55 -0700

    Add validation for photo presence on new user creation

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
   validates :password_digest, presence: true
   validates :session_token, presence: true, uniqueness: true
   validates :password, length: { minimum: 6 }, allow_nil: true
+  validates :photo, presence: true
 
   after_initialize :ensure_session_token
 
