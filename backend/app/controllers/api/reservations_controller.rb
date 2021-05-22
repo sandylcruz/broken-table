93ce765f40aacc37d98c454f0525eadb31d5dfec
Sandy Cruz <sandyleahcruz@icloud.com>
2021-05-22 14:45:37 -0700

    Refactor code to be shorter

--- backend/app/controllers/api/reservations_controller.rb
+++ backend/app/controllers/api/reservations_controller.rb
@@ -1,30 +1,18 @@
 # frozen_string_literal: true
 
 module Api
-  # rubocop:disable Metrics/AbcSize
-  # rubocop:disable Metrics/MethodLength
-  # rubocop:disable Layout/LineLength
   # Reservations Controller class
   class ReservationsController < ApplicationController
+    before_action :find_restaurant, only: [:create]
+    before_action :make_reservation, only: [:create]
+
     def create
-      restaurant = Restaurant.find_by(id: res_params[:restaurant_id])
-      render json: ['No restaurant found'], status: :unprocessable_entity unless restaurant
-
-      current_capacity_available = restaurant.is_space_available?(res_params[:date], res_params[:time_slot], res_params[:party_size])
-
-      unless current_capacity_available
+      if !@restaurant
+        render json: ['No restaurant found'], status: :unprocessable_entity
+      elsif !@restaurant.is_space_available?(res_params[:date], res_params[:time_slot], res_params[:party_size])
         render json: ['Reservation not created. Capacity exhausted.'], status: :unprocessable_entity
       end
-
-      @reservation = Reservation.new(restaurant_id: res_params[:restaurant_id],
-                                     date: Date.parse(res_params[:date]), time_slot: res_params[:time_slot],
-                                     party_size: res_params[:party_size], user_id: current_user.id)
-
-      if @reservation.save
-        render :show
-      else
-        render json: ['Reservation not saved'], status: :unprocessable_entity
-      end
+      @reservation.save ? render(:show) : render(json: ['Reservation not saved'], status: :unprocessable_entity)
     end
 
     def destroy
@@ -42,8 +30,15 @@ module Api
     def res_params
       params.require(:reservation).permit(:date, :party_size, :time_slot, :user_id, :restaurant_id)
     end
+
+    def make_reservation
+      @reservation = Reservation.new(restaurant_id: res_params[:restaurant_id],
+                                     date: Date.parse(res_params[:date]), time_slot: res_params[:time_slot],
+                                     party_size: res_params[:party_size], user_id: current_user.id)
+    end
+
+    def find_restaurant
+      @restaurant = Restaurant.find_by(id: res_params[:restaurant_id])
+    end
   end
 end
-# rubocop:enable Metrics/AbcSize
-# rubocop:enable Metrics/MethodLength
-# rubocop:enable Layout/LineLength
